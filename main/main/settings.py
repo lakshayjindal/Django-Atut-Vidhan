1bc23b7c9d0184c47d9a8a4343a96313b08e77f7
Lakshay Jindal <ljindal275@gmail.com>
2025-06-27 11:27:51 +0530

    Update settings.py

--- main/main/settings.py
+++ main/main/settings.py
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "main/static"]
+STATICFILES_DIRS = [BASE_DIR / "main/static", BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "static"
 DEBUG = True
 
