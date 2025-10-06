45524892b48b03ffcee44baa0963dbb4bdfd0da2
Lakshay Jindal <ljindal275@gmail.com>
2025-10-06 09:53:38 +0530

    updates

--- start.sh
+++ start.sh
@@ -11,4 +11,4 @@ python manage.py collectstatic --noinput
 
 echo "Starting Daphne ASGI server..."
 # Use the port provided by Railway
-exec daphne -b 0.0.0.0 -p ${PORT:-8000} main.asgi:application
+exec daphne -b 0.0.0.0 -p ${PORT:-8080} main.asgi:application
