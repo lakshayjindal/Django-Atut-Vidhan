6e2dd2420d7d552d823391bc64aaf806813646ab
Lakshay Jindal <ljindal275@gmail.com>
2025-10-06 08:53:54 +0530

    Added railway.toml

--- start.sh
+++ start.sh
@@ -1,17 +1,13 @@
 #!/usr/bin/env bash
-# Exit on any error
 set -o errexit
 
 cd main
 
-# ---- 1. Collect static files ----
 echo "📦 Collecting static files..."
 python3 manage.py collectstatic --noinput
 
-# ---- 2. Apply database migrations ----
 echo "🧩 Applying database migrations..."
 python3 manage.py migrate --noinput
 
-# ---- 3. Start ASGI server ----
 echo "🚀 Starting ASGI server (Daphne)..."
 daphne -b 0.0.0.0 -p $PORT main.asgi:application
